6fd72e238743042327ec650a01377c1b4018cf3f
Jean <Jean@Jean_Shiri-PC.dhcp.wustl.edu>
2016-10-12 19:15:16 -0500

    assignment5

---
+++ studios/studio6/Debounce/Debounce.ino
@@ -0,0 +1,56 @@
+
+
+
+const int buttonPin = 2;    
+const int ledPin = 13;      
+
+
+int ledState = HIGH;        
+int buttonState;             
+int lastButtonState = LOW; 
+
+
+long lastDebounceTime = 0;  
+long debounceDelay = 50;    
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  
+
+  
+  if (reading != lastButtonState) {
+    
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    
+
+    
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  
+  digitalWrite(ledPin, ledState);
+
+  
+  lastButtonState = reading;
+}
+
---
+++ studios/studio6/Mirror/Mirror.ino
@@ -0,0 +1,31 @@
+
+const int buttonPin = 2;     
+const int ledPin =  13;    
+int count=0; 
+
+
+int buttonState = 0;        
+
+void setup() {
+  
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  
+  buttonState = digitalRead(buttonPin);
+
+  
+  if (buttonState == HIGH) {
+    
+    digitalWrite(ledPin, HIGH);
+    count++;
+  } else {
+    
+    digitalWrite(ledPin, LOW);
+    
+  }
+  Serial.println(count);
+}
